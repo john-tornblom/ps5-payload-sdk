e8f9b3b647fb7c315f3c87e7341021271a9216c5
John Törnblom <john.tornblom@gmail.com>
2023-06-06 09:27:42 +0200

    add support for arbitrary syscalls
    Thanks for the idea, sleirsgoevy

---
+++ crt/syscall.c
@@ -0,0 +1,43 @@
+/* Copyright (C) 2023 John Törnblom
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING. If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#include "payload.h"
+
+
+static __attribute__ ((used)) long ptr_syscall;
+
+
+asm(".intel_syntax noprefix\n"
+    ".global syscall\n"
+    ".type syscall @function\n"
+    "syscall:\n"
+    "  mov rax, rdi\n"                      // sysno
+    "  mov rdi, rsi\n"                      // arg1
+    "  mov rsi, rdx\n"                      // arg2
+    "  mov rdx, rcx\n"                      // arg3
+    "  mov r10, r8\n"                       // arg4
+    "  mov r8,  r9\n"                       // arg5
+    "  mov r9,  qword ptr [rsp + 8]\n"      // arg6
+    "  jmp qword ptr [rip + ptr_syscall]\n" // syscall
+    "  ret\n"
+    );
+
+
+__attribute__((constructor(101))) static void
+constructor(const payload_args_t *args) {
+  *args->payloadout = args->sceKernelDlsym(0x2001, "getpid", &ptr_syscall);
+  ptr_syscall += 0xa; // jump directly to syscall instruction
+}
