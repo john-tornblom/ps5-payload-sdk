f9ec9e4b0a4574d7db904812f634601c6ccad16d
John Törnblom <john.tornblom@gmail.com>
2023-06-18 16:35:31 +0200

    cmake example: allow building from any working directory

--- samples/hello_cmake/build.sh
+++ samples/hello_cmake/build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+SCRIPTDIR="${BASH_SOURCE[0]}"
+SCRIPTDIR="$(dirname "${SCRIPTDIR}")"
 
 if [[ -z "$PS5_PAYLOAD_SDK" ]]; then
     echo "error: PS5_PAYLOAD_SDK is undefined"
     exit 1
 fi
 
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=$PS5_PAYLOAD_SDK/cmake/toolchain.cmake || exit 1
-make  -C build || exit 1
+cmake -B $SCRIPTDIR/build \
+      -DCMAKE_TOOLCHAIN_FILE=$PS5_PAYLOAD_SDK/cmake/toolchain.cmake \
+      $SCRIPTDIR || exit 1
+make  -C $SCRIPTDIR/build || exit 1
