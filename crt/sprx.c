9bee1bf985e2ff1b06ed20d74ee41bf30bd75f93
John Törnblom <john.tornblom@gmail.com>
2023-09-21 22:33:04 +0200

    Only assume that libkernel and libSceLibcInternal are loaded before the paqyload is deployed

--- crt/sprx.c
+++ crt/sprx.c
@@ -18,14 +18,17 @@ along with this program; see the file COPYING. If not, see
 
 
 /**
- * External ependencies.
+ * External dependencies.
  **/
 static int (*sceKernelDlsym)(int, const char*, void*) = 0;
 static int (*sceKernelStopUnloadModule)(int, unsigned long, const void*,
 					unsigned int, const void*, void*) = 0;
-static int (*sceSysmoduleLoadModuleByNameInternal)(const char*, unsigned long,
-						   const void*, unsigned int,
-						   void*) = 0;
+static int (*sceKernelLoadStartModule)(const char*, unsigned long,
+				       const void*, unsigned int,
+				       void*, int*) = 0;
+static const char* (*sceKernelGetFsSandboxRandomWord)(void) = 0;
+static int (*snprintf)(char *, unsigned long, const char *, ...) = 0;
+static int (*access)(const char*, int) = 0;
 
 
 /**
@@ -37,7 +40,7 @@ static int *sprx_error = 0;
 /**
  *
  **/
-__attribute__((constructor(101))) static void
+__attribute__((constructor(102))) static void
 sprx_constructor(const payload_args_t *args) {
   int error;
 
@@ -49,8 +52,21 @@ sprx_constructor(const payload_args_t *args) {
     *sprx_error = error;
     return;
   }
-  if((error=sceKernelDlsym(0x11, "sceSysmoduleLoadModuleByNameInternal",
-			   &sceSysmoduleLoadModuleByNameInternal))) {
+  if((error=sceKernelDlsym(0x2001, "sceKernelLoadStartModule",
+			   &sceKernelLoadStartModule))) {
+    *sprx_error = error;
+    return;
+  }
+  if((error=sceKernelDlsym(0x2001, "sceKernelGetFsSandboxRandomWord",
+			   &sceKernelGetFsSandboxRandomWord))) {
+    *sprx_error = error;
+    return;
+  }
+  if((error=sceKernelDlsym(0x2001, "access", &access))) {
+    *sprx_error = error;
+    return;
+  }
+  if((error=sceKernelDlsym(0x2, "snprintf", &snprintf))) {
     *sprx_error = error;
     return;
   }
@@ -77,15 +93,51 @@ sprx_dlsym(unsigned short handle, const char *symname, void *addr) {
  **/
 int
 sprx_dlopen(const char* libname, unsigned short *handle) {
-  unsigned int ret = sceSysmoduleLoadModuleByNameInternal(libname, 0, 0, 0, 0);
+  const char *sandbox_path = sceKernelGetFsSandboxRandomWord();
+  char path[0x100];
 
-  if(ret & 0xffff0000) {
-    *sprx_error = ret;
-    return ret;
-  } else {
-    *handle = ret & 0xffff;
+  if(sandbox_path) {
+    snprintf(path, sizeof(path), "/%s/priv_ex/lib/%s.sprx", sandbox_path, libname);
+    if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+      return 0;
+    }
+
+    snprintf(path, sizeof(path),"/%s/common_ex/lib/%s.sprx", sandbox_path, libname);
+    if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+      return 0;
+    }
+
+    snprintf(path, sizeof(path), "/%s/priv/lib/%s.sprx", sandbox_path, libname);
+    if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+      return 0;
+    }
+    snprintf(path, sizeof(path), "/%s/common/lib/%s.sprx", sandbox_path, libname);
+    if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+      return 0;
+    }
+  }
+
+  snprintf(path, sizeof(path),"/system_ex/priv_ex/lib/%s.sprx", libname);
+  if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
     return 0;
   }
+
+  snprintf(path, sizeof(path),"/system_ex/common_ex/lib/%s.sprx", libname);
+  if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+    return 0;
+  }
+
+  snprintf(path, sizeof(path),"/system/priv/lib/%s.sprx", libname);
+  if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, 0)) > 0) {
+    return 0;
+  }
+
+  snprintf(path, sizeof(path),"/system/common/lib/%s.sprx", libname);
+  if(!access(path, 0) && (*handle=sceKernelLoadStartModule(path, 0, 0, 0, 0, sprx_error)) > 0) {
+    return 0;
+  }
+
+  return -1;
 }
 
 
