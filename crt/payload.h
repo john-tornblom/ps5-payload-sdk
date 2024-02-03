5d0056428080fcc5334f94e8d209e16deff10e96
John Törnblom <john.tornblom@gmail.com>
2024-02-03 01:05:44 +0100

    cosmetics

--- crt/payload.h
+++ crt/payload.h
@@ -1,9 +1,22 @@
-#ifndef PS5SDK_PAYLOAD_H
-#define PS5SDK_PAYLOAD_H
+/* Copyright (C) 2024 John Törnblom
 
-/**
- * Payload arguments.
- **/
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING. If not, see
+<http://www.gnu.org/licenses/>.  */
+
+#pragma once
+
+
 typedef struct payload_args {
   int (*sceKernelDlsym)(int, const char*, void*);
   int  *rwpipe;
@@ -14,7 +27,3 @@ typedef struct payload_args {
 } payload_args_t;
 
 
-extern payload_args_t* __payload_args;
-
-
-#endif // PS5SDK_PAYLOAD_H
