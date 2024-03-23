4e2c95bbbf22904d8d53663e39ba099f2e544d5c
John Törnblom <john.tornblom@gmail.com>
2024-03-23 19:18:25 +0100

    ci: run cmake from the samples dir

--- samples/hello_cmake/build.sh
+++ samples/hello_cmake/build.sh
@@ -2,5 +2,6 @@
 SCRIPTDIR="${BASH_SOURCE[0]}"
 SCRIPTDIR="$(dirname "${SCRIPTDIR}")"
 
-cmake -B $SCRIPTDIR/build || exit 1
-make  -C $SCRIPTDIR/build || exit 1
+cd $SCRIPTDIR
+cmake -B build || exit 1
+make  -C build || exit 1
