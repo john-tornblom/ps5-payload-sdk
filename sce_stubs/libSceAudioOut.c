0383802f6ac3772548c574f660586d1895a9a9c1
John Törnblom <john.tornblom@gmail.com>
2024-04-07 01:01:18 +0200

    sce_stubs: add libSceAudioOut

---
+++ sce_stubs/libSceAudioOut.c
@@ -0,0 +1,492 @@
+
+asm(".global sceAudioDeviceControlGet\n"
+    ".type sceAudioDeviceControlGet @function\n"
+    "sceAudioDeviceControlGet:\n");
+
+asm(".global sceAudioDeviceControlSet\n"
+    ".type sceAudioDeviceControlSet @function\n"
+    "sceAudioDeviceControlSet:\n");
+
+asm(".global sceAudioOut2ArbitrationInitialize\n"
+    ".type sceAudioOut2ArbitrationInitialize @function\n"
+    "sceAudioOut2ArbitrationInitialize:\n");
+
+asm(".global sceAudioOut2ContextAdvance\n"
+    ".type sceAudioOut2ContextAdvance @function\n"
+    "sceAudioOut2ContextAdvance:\n");
+
+asm(".global sceAudioOut2ContextBedWrite\n"
+    ".type sceAudioOut2ContextBedWrite @function\n"
+    "sceAudioOut2ContextBedWrite:\n");
+
+asm(".global sceAudioOut2ContextCreate\n"
+    ".type sceAudioOut2ContextCreate @function\n"
+    "sceAudioOut2ContextCreate:\n");
+
+asm(".global sceAudioOut2ContextDestroy\n"
+    ".type sceAudioOut2ContextDestroy @function\n"
+    "sceAudioOut2ContextDestroy:\n");
+
+asm(".global sceAudioOut2ContextGetQueueLevel\n"
+    ".type sceAudioOut2ContextGetQueueLevel @function\n"
+    "sceAudioOut2ContextGetQueueLevel:\n");
+
+asm(".global sceAudioOut2ContextPush\n"
+    ".type sceAudioOut2ContextPush @function\n"
+    "sceAudioOut2ContextPush:\n");
+
+asm(".global sceAudioOut2ContextQueryMemory\n"
+    ".type sceAudioOut2ContextQueryMemory @function\n"
+    "sceAudioOut2ContextQueryMemory:\n");
+
+asm(".global sceAudioOut2ContextResetParam\n"
+    ".type sceAudioOut2ContextResetParam @function\n"
+    "sceAudioOut2ContextResetParam:\n");
+
+asm(".global sceAudioOut2ContextSetAttributes\n"
+    ".type sceAudioOut2ContextSetAttributes @function\n"
+    "sceAudioOut2ContextSetAttributes:\n");
+
+asm(".global sceAudioOut2DebugStateCtrl\n"
+    ".type sceAudioOut2DebugStateCtrl @function\n"
+    "sceAudioOut2DebugStateCtrl:\n");
+
+asm(".global sceAudioOut2DisableChat\n"
+    ".type sceAudioOut2DisableChat @function\n"
+    "sceAudioOut2DisableChat:\n");
+
+asm(".global sceAudioOut2EnableChat\n"
+    ".type sceAudioOut2EnableChat @function\n"
+    "sceAudioOut2EnableChat:\n");
+
+asm(".global sceAudioOut2GetIpcHandle\n"
+    ".type sceAudioOut2GetIpcHandle @function\n"
+    "sceAudioOut2GetIpcHandle:\n");
+
+asm(".global sceAudioOut2GetSpeakerArrayAmbisonicsCoefficients\n"
+    ".type sceAudioOut2GetSpeakerArrayAmbisonicsCoefficients @function\n"
+    "sceAudioOut2GetSpeakerArrayAmbisonicsCoefficients:\n");
+
+asm(".global sceAudioOut2GetSpeakerArrayCoefficients\n"
+    ".type sceAudioOut2GetSpeakerArrayCoefficients @function\n"
+    "sceAudioOut2GetSpeakerArrayCoefficients:\n");
+
+asm(".global sceAudioOut2GetSpeakerArrayMemorySize\n"
+    ".type sceAudioOut2GetSpeakerArrayMemorySize @function\n"
+    "sceAudioOut2GetSpeakerArrayMemorySize:\n");
+
+asm(".global sceAudioOut2GetSpeakerInfo\n"
+    ".type sceAudioOut2GetSpeakerInfo @function\n"
+    "sceAudioOut2GetSpeakerInfo:\n");
+
+asm(".global sceAudioOut2GetSystemState\n"
+    ".type sceAudioOut2GetSystemState @function\n"
+    "sceAudioOut2GetSystemState:\n");
+
+asm(".global sceAudioOut2GetSystemStateEx\n"
+    ".type sceAudioOut2GetSystemStateEx @function\n"
+    "sceAudioOut2GetSystemStateEx:\n");
+
+asm(".global sceAudioOut2Initialize\n"
+    ".type sceAudioOut2Initialize @function\n"
+    "sceAudioOut2Initialize:\n");
+
+asm(".global sceAudioOut2InitializeEx\n"
+    ".type sceAudioOut2InitializeEx @function\n"
+    "sceAudioOut2InitializeEx:\n");
+
+asm(".global sceAudioOut2LoContextAdvance\n"
+    ".type sceAudioOut2LoContextAdvance @function\n"
+    "sceAudioOut2LoContextAdvance:\n");
+
+asm(".global sceAudioOut2LoContextCreate\n"
+    ".type sceAudioOut2LoContextCreate @function\n"
+    "sceAudioOut2LoContextCreate:\n");
+
+asm(".global sceAudioOut2LoContextDestroy\n"
+    ".type sceAudioOut2LoContextDestroy @function\n"
+    "sceAudioOut2LoContextDestroy:\n");
+
+asm(".global sceAudioOut2LoContextGetQueueLevel\n"
+    ".type sceAudioOut2LoContextGetQueueLevel @function\n"
+    "sceAudioOut2LoContextGetQueueLevel:\n");
+
+asm(".global sceAudioOut2LoContextPush\n"
+    ".type sceAudioOut2LoContextPush @function\n"
+    "sceAudioOut2LoContextPush:\n");
+
+asm(".global sceAudioOut2LoContextQueryMemory\n"
+    ".type sceAudioOut2LoContextQueryMemory @function\n"
+    "sceAudioOut2LoContextQueryMemory:\n");
+
+asm(".global sceAudioOut2LoContextSetAttributes\n"
+    ".type sceAudioOut2LoContextSetAttributes @function\n"
+    "sceAudioOut2LoContextSetAttributes:\n");
+
+asm(".global sceAudioOut2LoInit\n"
+    ".type sceAudioOut2LoInit @function\n"
+    "sceAudioOut2LoInit:\n");
+
+asm(".global sceAudioOut2LoPortCreate\n"
+    ".type sceAudioOut2LoPortCreate @function\n"
+    "sceAudioOut2LoPortCreate:\n");
+
+asm(".global sceAudioOut2LoPortDestroy\n"
+    ".type sceAudioOut2LoPortDestroy @function\n"
+    "sceAudioOut2LoPortDestroy:\n");
+
+asm(".global sceAudioOut2LoPortGetState\n"
+    ".type sceAudioOut2LoPortGetState @function\n"
+    "sceAudioOut2LoPortGetState:\n");
+
+asm(".global sceAudioOut2LoPortSetAttributes\n"
+    ".type sceAudioOut2LoPortSetAttributes @function\n"
+    "sceAudioOut2LoPortSetAttributes:\n");
+
+asm(".global sceAudioOut2LoTerminate\n"
+    ".type sceAudioOut2LoTerminate @function\n"
+    "sceAudioOut2LoTerminate:\n");
+
+asm(".global sceAudioOut2MasteringGetState\n"
+    ".type sceAudioOut2MasteringGetState @function\n"
+    "sceAudioOut2MasteringGetState:\n");
+
+asm(".global sceAudioOut2MasteringInit\n"
+    ".type sceAudioOut2MasteringInit @function\n"
+    "sceAudioOut2MasteringInit:\n");
+
+asm(".global sceAudioOut2MasteringInitEx\n"
+    ".type sceAudioOut2MasteringInitEx @function\n"
+    "sceAudioOut2MasteringInitEx:\n");
+
+asm(".global sceAudioOut2MasteringSetParam\n"
+    ".type sceAudioOut2MasteringSetParam @function\n"
+    "sceAudioOut2MasteringSetParam:\n");
+
+asm(".global sceAudioOut2MasteringTerm\n"
+    ".type sceAudioOut2MasteringTerm @function\n"
+    "sceAudioOut2MasteringTerm:\n");
+
+asm(".global sceAudioOut2MbusInit\n"
+    ".type sceAudioOut2MbusInit @function\n"
+    "sceAudioOut2MbusInit:\n");
+
+asm(".global sceAudioOut2PortCreate\n"
+    ".type sceAudioOut2PortCreate @function\n"
+    "sceAudioOut2PortCreate:\n");
+
+asm(".global sceAudioOut2PortCreateEx\n"
+    ".type sceAudioOut2PortCreateEx @function\n"
+    "sceAudioOut2PortCreateEx:\n");
+
+asm(".global sceAudioOut2PortDestroy\n"
+    ".type sceAudioOut2PortDestroy @function\n"
+    "sceAudioOut2PortDestroy:\n");
+
+asm(".global sceAudioOut2PortGetState\n"
+    ".type sceAudioOut2PortGetState @function\n"
+    "sceAudioOut2PortGetState:\n");
+
+asm(".global sceAudioOut2PortRegister\n"
+    ".type sceAudioOut2PortRegister @function\n"
+    "sceAudioOut2PortRegister:\n");
+
+asm(".global sceAudioOut2PortSetAttributes\n"
+    ".type sceAudioOut2PortSetAttributes @function\n"
+    "sceAudioOut2PortSetAttributes:\n");
+
+asm(".global sceAudioOut2PortUnregister\n"
+    ".type sceAudioOut2PortUnregister @function\n"
+    "sceAudioOut2PortUnregister:\n");
+
+asm(".global sceAudioOut2ReportRegisterHandler\n"
+    ".type sceAudioOut2ReportRegisterHandler @function\n"
+    "sceAudioOut2ReportRegisterHandler:\n");
+
+asm(".global sceAudioOut2ReportUnregisterHandler\n"
+    ".type sceAudioOut2ReportUnregisterHandler @function\n"
+    "sceAudioOut2ReportUnregisterHandler:\n");
+
+asm(".global sceAudioOut2SetSystemDebugState\n"
+    ".type sceAudioOut2SetSystemDebugState @function\n"
+    "sceAudioOut2SetSystemDebugState:\n");
+
+asm(".global sceAudioOut2SpeakerArrayCreate\n"
+    ".type sceAudioOut2SpeakerArrayCreate @function\n"
+    "sceAudioOut2SpeakerArrayCreate:\n");
+
+asm(".global sceAudioOut2SpeakerArrayDestroy\n"
+    ".type sceAudioOut2SpeakerArrayDestroy @function\n"
+    "sceAudioOut2SpeakerArrayDestroy:\n");
+
+asm(".global sceAudioOut2Terminate\n"
+    ".type sceAudioOut2Terminate @function\n"
+    "sceAudioOut2Terminate:\n");
+
+asm(".global sceAudioOut2UserCreate\n"
+    ".type sceAudioOut2UserCreate @function\n"
+    "sceAudioOut2UserCreate:\n");
+
+asm(".global sceAudioOut2UserCreateEx\n"
+    ".type sceAudioOut2UserCreateEx @function\n"
+    "sceAudioOut2UserCreateEx:\n");
+
+asm(".global sceAudioOut2UserDestroy\n"
+    ".type sceAudioOut2UserDestroy @function\n"
+    "sceAudioOut2UserDestroy:\n");
+
+asm(".global sceAudioOut2UserGetSupportedAttributes\n"
+    ".type sceAudioOut2UserGetSupportedAttributes @function\n"
+    "sceAudioOut2UserGetSupportedAttributes:\n");
+
+asm(".global sceAudioOutArbitrationInitialize\n"
+    ".type sceAudioOutArbitrationInitialize @function\n"
+    "sceAudioOutArbitrationInitialize:\n");
+
+asm(".global sceAudioOutAssignHRTF\n"
+    ".type sceAudioOutAssignHRTF @function\n"
+    "sceAudioOutAssignHRTF:\n");
+
+asm(".global sceAudioOutAttachToApplicationByPid\n"
+    ".type sceAudioOutAttachToApplicationByPid @function\n"
+    "sceAudioOutAttachToApplicationByPid:\n");
+
+asm(".global sceAudioOutChangeAppModuleState\n"
+    ".type sceAudioOutChangeAppModuleState @function\n"
+    "sceAudioOutChangeAppModuleState:\n");
+
+asm(".global sceAudioOutClose\n"
+    ".type sceAudioOutClose @function\n"
+    "sceAudioOutClose:\n");
+
+asm(".global sceAudioOutDetachFromApplicationByPid\n"
+    ".type sceAudioOutDetachFromApplicationByPid @function\n"
+    "sceAudioOutDetachFromApplicationByPid:\n");
+
+asm(".global sceAudioOutDeviceIdOpen\n"
+    ".type sceAudioOutDeviceIdOpen @function\n"
+    "sceAudioOutDeviceIdOpen:\n");
+
+asm(".global sceAudioOutEnable3DAudioForUser\n"
+    ".type sceAudioOutEnable3DAudioForUser @function\n"
+    "sceAudioOutEnable3DAudioForUser:\n");
+
+asm(".global sceAudioOutExClose\n"
+    ".type sceAudioOutExClose @function\n"
+    "sceAudioOutExClose:\n");
+
+asm(".global sceAudioOutExConfigureOutput\n"
+    ".type sceAudioOutExConfigureOutput @function\n"
+    "sceAudioOutExConfigureOutput:\n");
+
+asm(".global sceAudioOutExConfigureOutputMode\n"
+    ".type sceAudioOutExConfigureOutputMode @function\n"
+    "sceAudioOutExConfigureOutputMode:\n");
+
+asm(".global sceAudioOutExGetLastOutputTime\n"
+    ".type sceAudioOutExGetLastOutputTime @function\n"
+    "sceAudioOutExGetLastOutputTime:\n");
+
+asm(".global sceAudioOutExGetMonitorInfo\n"
+    ".type sceAudioOutExGetMonitorInfo @function\n"
+    "sceAudioOutExGetMonitorInfo:\n");
+
+asm(".global sceAudioOutExGetOutputInfo\n"
+    ".type sceAudioOutExGetOutputInfo @function\n"
+    "sceAudioOutExGetOutputInfo:\n");
+
+asm(".global sceAudioOutExGetSystemInfo\n"
+    ".type sceAudioOutExGetSystemInfo @function\n"
+    "sceAudioOutExGetSystemInfo:\n");
+
+asm(".global sceAudioOutExOpen\n"
+    ".type sceAudioOutExOpen @function\n"
+    "sceAudioOutExOpen:\n");
+
+asm(".global sceAudioOutExPtClose\n"
+    ".type sceAudioOutExPtClose @function\n"
+    "sceAudioOutExPtClose:\n");
+
+asm(".global sceAudioOutExPtGetLastOutputTime\n"
+    ".type sceAudioOutExPtGetLastOutputTime @function\n"
+    "sceAudioOutExPtGetLastOutputTime:\n");
+
+asm(".global sceAudioOutExPtOpen\n"
+    ".type sceAudioOutExPtOpen @function\n"
+    "sceAudioOutExPtOpen:\n");
+
+asm(".global sceAudioOutExSystemInfoIsSupportedAudioOutExMode\n"
+    ".type sceAudioOutExSystemInfoIsSupportedAudioOutExMode @function\n"
+    "sceAudioOutExSystemInfoIsSupportedAudioOutExMode:\n");
+
+asm(".global sceAudioOutGetFocusEnablePid\n"
+    ".type sceAudioOutGetFocusEnablePid @function\n"
+    "sceAudioOutGetFocusEnablePid:\n");
+
+asm(".global sceAudioOutGetHandleStatusInfo\n"
+    ".type sceAudioOutGetHandleStatusInfo @function\n"
+    "sceAudioOutGetHandleStatusInfo:\n");
+
+asm(".global sceAudioOutGetInfo\n"
+    ".type sceAudioOutGetInfo @function\n"
+    "sceAudioOutGetInfo:\n");
+
+asm(".global sceAudioOutGetInfoOpenNum\n"
+    ".type sceAudioOutGetInfoOpenNum @function\n"
+    "sceAudioOutGetInfoOpenNum:\n");
+
+asm(".global sceAudioOutGetLastOutputTime\n"
+    ".type sceAudioOutGetLastOutputTime @function\n"
+    "sceAudioOutGetLastOutputTime:\n");
+
+asm(".global sceAudioOutGetPortState\n"
+    ".type sceAudioOutGetPortState @function\n"
+    "sceAudioOutGetPortState:\n");
+
+asm(".global sceAudioOutGetSimulatedBusUsableStatusByBusType\n"
+    ".type sceAudioOutGetSimulatedBusUsableStatusByBusType @function\n"
+    "sceAudioOutGetSimulatedBusUsableStatusByBusType:\n");
+
+asm(".global sceAudioOutGetSimulatedHandleStatusInfo\n"
+    ".type sceAudioOutGetSimulatedHandleStatusInfo @function\n"
+    "sceAudioOutGetSimulatedHandleStatusInfo:\n");
+
+asm(".global sceAudioOutGetSimulatedHandleStatusInfo2\n"
+    ".type sceAudioOutGetSimulatedHandleStatusInfo2 @function\n"
+    "sceAudioOutGetSimulatedHandleStatusInfo2:\n");
+
+asm(".global sceAudioOutGetSystemState\n"
+    ".type sceAudioOutGetSystemState @function\n"
+    "sceAudioOutGetSystemState:\n");
+
+asm(".global sceAudioOutInit\n"
+    ".type sceAudioOutInit @function\n"
+    "sceAudioOutInit:\n");
+
+asm(".global sceAudioOutMasteringGetState\n"
+    ".type sceAudioOutMasteringGetState @function\n"
+    "sceAudioOutMasteringGetState:\n");
+
+asm(".global sceAudioOutMasteringInit\n"
+    ".type sceAudioOutMasteringInit @function\n"
+    "sceAudioOutMasteringInit:\n");
+
+asm(".global sceAudioOutMasteringSetParam\n"
+    ".type sceAudioOutMasteringSetParam @function\n"
+    "sceAudioOutMasteringSetParam:\n");
+
+asm(".global sceAudioOutMasteringTerm\n"
+    ".type sceAudioOutMasteringTerm @function\n"
+    "sceAudioOutMasteringTerm:\n");
+
+asm(".global sceAudioOutOpen\n"
+    ".type sceAudioOutOpen @function\n"
+    "sceAudioOutOpen:\n");
+
+asm(".global sceAudioOutOpenEx\n"
+    ".type sceAudioOutOpenEx @function\n"
+    "sceAudioOutOpenEx:\n");
+
+asm(".global sceAudioOutOutput\n"
+    ".type sceAudioOutOutput @function\n"
+    "sceAudioOutOutput:\n");
+
+asm(".global sceAudioOutOutputs\n"
+    ".type sceAudioOutOutputs @function\n"
+    "sceAudioOutOutputs:\n");
+
+asm(".global sceAudioOutPtClose\n"
+    ".type sceAudioOutPtClose @function\n"
+    "sceAudioOutPtClose:\n");
+
+asm(".global sceAudioOutPtGetLastOutputTime\n"
+    ".type sceAudioOutPtGetLastOutputTime @function\n"
+    "sceAudioOutPtGetLastOutputTime:\n");
+
+asm(".global sceAudioOutPtOpen\n"
+    ".type sceAudioOutPtOpen @function\n"
+    "sceAudioOutPtOpen:\n");
+
+asm(".global sceAudioOutSetAllMute\n"
+    ".type sceAudioOutSetAllMute @function\n"
+    "sceAudioOutSetAllMute:\n");
+
+asm(".global sceAudioOutSetCaesarVolume\n"
+    ".type sceAudioOutSetCaesarVolume @function\n"
+    "sceAudioOutSetCaesarVolume:\n");
+
+asm(".global sceAudioOutSetHeadphoneOutMode\n"
+    ".type sceAudioOutSetHeadphoneOutMode @function\n"
+    "sceAudioOutSetHeadphoneOutMode:\n");
+
+asm(".global sceAudioOutSetMainOutput\n"
+    ".type sceAudioOutSetMainOutput @function\n"
+    "sceAudioOutSetMainOutput:\n");
+
+asm(".global sceAudioOutSetMixLevelPadSpk\n"
+    ".type sceAudioOutSetMixLevelPadSpk @function\n"
+    "sceAudioOutSetMixLevelPadSpk:\n");
+
+asm(".global sceAudioOutSetPadJackVolume\n"
+    ".type sceAudioOutSetPadJackVolume @function\n"
+    "sceAudioOutSetPadJackVolume:\n");
+
+asm(".global sceAudioOutSetPadSpkVolume\n"
+    ".type sceAudioOutSetPadSpkVolume @function\n"
+    "sceAudioOutSetPadSpkVolume:\n");
+
+asm(".global sceAudioOutSetRecMode\n"
+    ".type sceAudioOutSetRecMode @function\n"
+    "sceAudioOutSetRecMode:\n");
+
+asm(".global sceAudioOutSetSystemDebugState\n"
+    ".type sceAudioOutSetSystemDebugState @function\n"
+    "sceAudioOutSetSystemDebugState:\n");
+
+asm(".global sceAudioOutSetUsbVolume\n"
+    ".type sceAudioOutSetUsbVolume @function\n"
+    "sceAudioOutSetUsbVolume:\n");
+
+asm(".global sceAudioOutSetVolume\n"
+    ".type sceAudioOutSetVolume @function\n"
+    "sceAudioOutSetVolume:\n");
+
+asm(".global sceAudioOutSetVolumeDown\n"
+    ".type sceAudioOutSetVolumeDown @function\n"
+    "sceAudioOutSetVolumeDown:\n");
+
+asm(".global sceAudioOutSysClose\n"
+    ".type sceAudioOutSysClose @function\n"
+    "sceAudioOutSysClose:\n");
+
+asm(".global sceAudioOutSysConfigureOutput\n"
+    ".type sceAudioOutSysConfigureOutput @function\n"
+    "sceAudioOutSysConfigureOutput:\n");
+
+asm(".global sceAudioOutSysConfigureOutputMode\n"
+    ".type sceAudioOutSysConfigureOutputMode @function\n"
+    "sceAudioOutSysConfigureOutputMode:\n");
+
+asm(".global sceAudioOutSysGetHdmiMonitorInfo\n"
+    ".type sceAudioOutSysGetHdmiMonitorInfo @function\n"
+    "sceAudioOutSysGetHdmiMonitorInfo:\n");
+
+asm(".global sceAudioOutSysGetMonitorInfo\n"
+    ".type sceAudioOutSysGetMonitorInfo @function\n"
+    "sceAudioOutSysGetMonitorInfo:\n");
+
+asm(".global sceAudioOutSysGetOutputInfo\n"
+    ".type sceAudioOutSysGetOutputInfo @function\n"
+    "sceAudioOutSysGetOutputInfo:\n");
+
+asm(".global sceAudioOutSysGetSystemInfo\n"
+    ".type sceAudioOutSysGetSystemInfo @function\n"
+    "sceAudioOutSysGetSystemInfo:\n");
+
+asm(".global sceAudioOutSysHdmiMonitorInfoIsSupportedAudioOutMode\n"
+    ".type sceAudioOutSysHdmiMonitorInfoIsSupportedAudioOutMode @function\n"
+    "sceAudioOutSysHdmiMonitorInfoIsSupportedAudioOutMode:\n");
+
+asm(".global sceAudioOutSysOpen\n"
+    ".type sceAudioOutSysOpen @function\n"
+    "sceAudioOutSysOpen:\n");
